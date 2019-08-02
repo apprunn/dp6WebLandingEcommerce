58300a4b3e17739182e049348749b3926f5672da
John Doe <airomerick9817@gmail.com>
2019-08-02 10:22:20 -0500

    maquetar componente chat

---
+++ src/components/shared/buttons/component-chat.vue
@@ -0,0 +1,50 @@
+<template>
+	<div class="component-chat-opacity">
+		<div class="circle-opacity">
+			<div class="circle-conversation" :style="`background-color: ${background}`">
+				<img :src="icon" alt="">
+			</div>
+		</div>
+	</div>
+</template>
+
+<script>
+export default {
+	name: 'component-chat',
+	props: {
+		icon: String,
+		background: String,
+	},
+};
+</script>
+
+<style lang="scss" scoped>
+.circle-conversation {
+	align-items: center;
+	border-radius: 50%;
+	display: flex;
+	height: 82px;
+	justify-content: center;
+	width: 82px;
+}
+
+.circle-opacity {
+	align-items: center;
+	background: rgba(244, 43, 23, 0.5);
+	border-radius: 50%;
+	display: flex;
+	height: 94px;
+	justify-content: center;
+	width: 94px;
+}
+
+.component-chat-opacity {
+	align-items: center;
+	background: rgba(244, 43, 23, 0.3);
+	border-radius: 50%;
+	display: flex;
+	height: 106px;
+	justify-content: center;
+	width: 106px;
+}
+</style>
--- src/pages/page-home.vue
+++ src/pages/page-home.vue
@@ -1,11 +1,27 @@
 <template>
 	<layout-admin>
 		<router-view></router-view>
+		<component-chat
+		icon="/static/img/icons/icon-conversation.svg"
+		:background="baseColor"
+		></component-chat>
 	</layout-admin>
 </template>
 
 <script>
+const componentChat = () => import('@/components/shared/buttons/component-chat');
+
+function data() {
+	return {
+		baseColor: process.env.COLOR_BASE,
+	};
+}
+
 export default {
+	data,
 	name: 'page-home',
+	components: {
+		componentChat,
+	},
 };
 </script>
