39d49451a7b043dcc2b067bb40b411e3853cb543
Luis Huamani <elluis1520@gmail.com>
2021-06-04 08:43:19 -0500

    remove rules eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -36,7 +36,6 @@ module.exports = {
     // allow debugger during development
 		'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0,
 		'no-tabs': 0,
-		'indent': ['error', 'tab'],
-    "linebreak-style": 0
+		'indent': ['error', 'tab']
   }
 }
