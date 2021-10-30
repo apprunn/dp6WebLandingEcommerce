2aaaf40ce878fc7230c8d4a4b4b9180f5a0058e3
airomerick98 <airomerick9817@gmail.com>
2021-10-30 17:05:37 -0500

    agregar query shoper

--- src/router/detail-product.js
+++ src/router/detail-product.js
@@ -1,5 +1,5 @@
 const route = {
-	path: '/:id/detalle-producto',
+	path: '/:id/detalle-producto/:shoper?',
 	name: 'detail-product',
 	component: () => import(/* webpackChunkName: "page-detail-product" */'@/pages/page-detail-product'),
 	props: true,
