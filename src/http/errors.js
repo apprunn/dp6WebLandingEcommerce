b9703bd40ab1718bfa98fe103037a01df5d32f3b
Ivan495 <canarivan495@gmail.com>
2021-11-16 16:36:22 -0500

    feat:(BUG) Cambio de mensaje para ordenes ya finalizadas

--- src/http/errors.js
+++ src/http/errors.js
@@ -2,7 +2,7 @@ export default function (code) {
 	if (code === 'USER_EXISTS') {
 		return 'El usuario ya existe';
 	} else if (code.data.message === 'NO_UPDATE_BECAUSE_ORDER_FINALIZED') {
-		return 'La orden ya a sido finalizada';
+		return 'La orden se encuentra finalizada';
 	}
 	return 'Verifique que completó los datos requeridos';
 }
